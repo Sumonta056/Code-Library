cbda330d8f93ec63196ed11b3990a60f32322a0a
Sumonta Saha Mridul <sumontasaha80@gmail.com>
2022-09-06 08:30:36 +0600

    Co-Prime

---
+++ DS Algo + CP/Number Theory/CoPrime/CheckTwoNumberCoPrime_OrNot.cpp
@@ -0,0 +1,58 @@
+//        ** Sumonta Saha Mridul **                                    SWE - SUST
+/*
+
+ *        ######    ##     ##  ##     ##   #######   ##    ##   ########      ###
+ !       ##    ##   ##     ##  ###   ###  ##     ##  ###   ##      ##        ## ##
+ ?       ##         ##     ##  #### ####  ##     ##  ####  ##      ##       ##   ##
+ *        ######    ##     ##  ## ### ##  ##     ##  ## ## ##      ##      ##     ##
+ !             ##   ##     ##  ##     ##  ##     ##  ##  ####      ##      #########
+ ?       ##    ##   ##     ##  ##     ##  ##     ##  ##   ###      ##      ##     ##
+ *        ######     #######   ##     ##   #######   ##    ##      ##      ##     ##
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define sp " "
+#define enter cout << endl;
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+
+    else
+        return gcd(b, a % b);
+}
+
+void coPrime(ll a, ll b)
+{
+    if (gcd(a, b) == 1)
+    {
+        cout << "Co-Prime" << endl;
+    }
+
+    else
+        cout << "Not Co-Prime" << endl;
+}
+
+int main()
+{
+    fast;
+
+    ll a = 99999999;
+    ll b = 3;
+    coPrime(a, b);
+}
+
+/*
+ * Time Complexity: O(log(max(a,b)))
+
+Auxiliary Space: O(1)
+*/
---
+++ DS Algo + CP/Number Theory/CoPrime/WhatCoprime.cpp
@@ -0,0 +1,8 @@
+/*
+
+* What is coprime?
+
+? If gcd of two value is 1 : then those two numbers are coprime.
+
+
+*/
